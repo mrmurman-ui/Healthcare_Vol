32e6771818fb146ef1171217ed425a69e7be89a7
mrmurman-ui <mr_murman@gmail.com>
2026-06-26 11:45:44 +0700

    Prepare for Streamlit deployment

--- RUN_ME.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo ================================================
-echo  Run this from Healthcare_Production_02 folder
-echo ================================================
-cd /d "%~dp0"
-py -3.12 scripts\patch_and_verify.py
-pause
